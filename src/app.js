281c62f23d7526f7aa0580dcced07a4bb6cc9970
Bernardo Rodrigues <bernardohmr@gmail.com>
2020-06-15 23:12:35 -0300

    Implementation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const uuid = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,69 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid.uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(el => el.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ message: "Invalid ID" });
+  }
+
+  const repository = repositories[repoIndex];
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(el => el.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ message: "Invalid ID" });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(el => el.id === id);
+  if (repoIndex < 0) {
+    return response.status(400).json({ message: "Invalid ID" });
+  }
+  const repository = repositories[repoIndex];
+  repository.likes ++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
